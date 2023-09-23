1f1283962fbafbce57523dbd4f452b1c7dba7b28
Josh Watley <watleyjosh@gmail.com>
2023-09-23 18:43:20 +0100

    Initial Commit

---
+++ src/main/java/com/jwtly10/discountFinder/DiscountFinderApplication.java
@@ -0,0 +1,13 @@
+package com.jwtly10.discountFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwtly10/discountFinder/DiscountFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtly10.discountFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
