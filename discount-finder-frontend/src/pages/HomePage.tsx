37f2d42cf502c441114eef028f9023ba80192995
Josh Watley <watleyjosh@gmail.com>
2023-10-05 12:49:10 +0100

    Adding home page

--- discount-finder-frontend/src/pages/HomePage.tsx
+++ discount-finder-frontend/src/pages/HomePage.tsx
@@ -1,5 +1,24 @@
+import { Container } from 'react-bootstrap'
+
 function HomePage() {
-    return <h1>Home Page</h1>
+    return (
+        <Container className="d-flex mt-5 flex-column justify-content-center align-items-center">
+            <h1>Fragrance Finder UK</h1>
+            <p>Discover discounted perfumes from a range of stores.</p>
+            <div className="d-flex flex-column align-items-center">
+                <h4>
+                    <a href="/products/mens" className="text-decoration-none">
+                        Browse Mens Range
+                    </a>
+                </h4>
+                <h4>
+                    <a href="/products/womens" className="text-decoration-none">
+                        Browse Womens Range
+                    </a>
+                </h4>
+            </div>
+        </Container>
+    )
 }
 
 export default HomePage
