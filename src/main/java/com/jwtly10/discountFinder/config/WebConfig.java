49c386cc736a465fdd720c5d56177b2ac8bb8b80
Josh Watley <watleyjosh@gmail.com>
2023-10-05 12:54:20 +0100

    Adding prod cors url

--- src/main/java/com/jwtly10/discountFinder/config/WebConfig.java
+++ src/main/java/com/jwtly10/discountFinder/config/WebConfig.java
@@ -6,14 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-            .allowedOrigins("http://localhost:5173", "https://aquamarine-boba-2b13ea.netlify.app")
-            .allowedMethods("GET");
+                .allowedOrigins("http://localhost:5173", "https://aquamarine-boba-2b13ea.netlify.app", "https://fragrancefinder.uk")
+                .allowedMethods("GET");
     }
 
-    
+
 }
