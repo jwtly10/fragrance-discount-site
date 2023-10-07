86eb1927278aff04384b184ac31e79b199f0d9e4
Josh Watley <watleyjosh@gmail.com>
2023-10-07 18:13:29 +0100

    Adding some intro text when loading products

---
+++ discount-finder-frontend/src/components/PlaceholderTemp.tsx
@@ -0,0 +1,23 @@
+import { Col } from 'react-bootstrap'
+import PlaceholderProduct from './PlaceholderProductList'
+
+function PlaceholderTemp() {
+    return (
+        <>
+            <p className="text-warning text-center">
+                {' '}
+                The first load might take up to 10 seconds... We have thousands
+                of products to process to find you the best deal🤭
+            </p>
+            {[...Array(12)].map((_, index: number) => {
+                return (
+                    <Col key={index} className="mb-4">
+                        <PlaceholderProduct />
+                    </Col>
+                )
+            })}
+        </>
+    )
+}
+
+export default PlaceholderTemp
